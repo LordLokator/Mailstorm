cd1609029c774aa81b1a82d5339cfd21b510863d
LordLokator <70625153+LordLokator@users.noreply.github.com>
2025-08-19 13:59:20 +0200

    moved template here

--- config.py
+++ config.py
@@ -1,22 +1,30 @@
+from textwrap import dedent
 from langchain_ollama.llms import OllamaLLM
+from langchain.prompts import PromptTemplate
 
 # Ollama params.
 # Set model here.
 # If remote Ollama server, set URL here.
 ollama_url = "http://localhost:11434/"
-model_name = "llama3.2:1b"
+model_name = "llama3.1:latest"
+
+
+output_format = """
+    Output must exactly be thesetwo lines with the appropriate answers in them:
+    - Blocker found: [Yes/No]
+    - Justification: [short text]
+    """
 
 # Initial prompt for the LLM.
-system_prompt = "You are a senior project manager specializing in risk detection in project communications. \
-                Your only task: detect potential blockers in the conversation. \
-                Disregard anything non-work related.\
-                A blocker is anything that delays progress, causes confusion, or requires escalation.  \
-                Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,  \
-                resource constraints, lack of approvals, misaligned deadlines etc. \
-                Do NOT generate more messages, your role is only to evaluate the conversation! \
-                Output format (always): \
-                - Blocker found: [Yes/No] \
-                - Justification: [short text]"
+system_prompt = """
+    You are a senior project manager specializing in risk detection in project communications.
+    Your only task: detect potential blockers in the conversation.
+    Disregard anything non-work related.
+    A blocker is anything that delays progress, causes confusion, or requires escalation.
+    Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,
+    resource constraints, lack of approvals, misaligned deadlines etc.
+    Do NOT generate more messages, your role is only to evaluate the conversation!
+"""
 
 # NOTE: for other LLMs (e.g OpenAI),
 # use other LangChain classes.
@@ -29,4 +37,21 @@ model = OllamaLLM(
     base_url=ollama_url,
 
     temperature=0  # reproducibility
+)
+
+
+template = PromptTemplate(
+    input_variables=["system", "conversation"],
+    template=dedent(
+        """
+        [SYSTEM]
+        {system}
+
+        [CONVERSATION]
+        {conversation}
+
+        [OUTPUT]
+        {output_format}
+        """
+    )
 )

70edb4575480b5abd8af648449a495006352ebf0
LordLokator <70625153+LordLokator@users.noreply.github.com>
2025-08-19 11:19:09 +0200

    notes on objects

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from langchain_ollama.llms import OllamaLLM
 
+# Ollama params.
+# Set model here.
+# If remote Ollama server, set URL here.
 ollama_url = "http://localhost:11434/"
-
 model_name = "llama3.2:1b"
 
+# Initial prompt for the LLM.
 system_prompt = "You are a senior project manager specializing in risk detection in project communications. \
                 Your only task: detect potential blockers in the conversation. \
                 Disregard anything non-work related.\
@@ -15,6 +18,11 @@ system_prompt = "You are a senior project manager specializing in risk detection
                 - Blocker found: [Yes/No] \
                 - Justification: [short text]"
 
+# NOTE: for other LLMs (e.g OpenAI),
+# use other LangChain classes.
+# These usually require API keys.
+# e.g use this import:
+# from langchain_community.llms.openai import OpenAIChat
 model = OllamaLLM(
     # specified in config
     model=model_name,

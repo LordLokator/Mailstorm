6aa9d8f4f3eced08ce64fa73130fcc516143b8a7
LordLokator <70625153+LordLokator@users.noreply.github.com>
2025-08-21 14:18:25 +0200

    moved prompts and templating to prompts.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from textwrap import dedent
 from langchain_ollama.llms import OllamaLLM
-from langchain.prompts import PromptTemplate
 
 # Ollama params.
 # Set model here.
@@ -8,23 +7,6 @@ from langchain.prompts import PromptTemplate
 ollama_url = "http://localhost:11434/"
 model_name = "llama3.1:latest"
 
-
-output_format = """
-    Output must exactly be thesetwo lines with the appropriate answers in them:
-    - Blocker found: [Yes/No]
-    - Justification: [short text]
-    """
-
-# Initial prompt for the LLM.
-system_prompt = """
-    You are a senior project manager specializing in risk detection in project communications.
-    Your only task: detect potential blockers in the conversation.
-    Disregard anything non-work related.
-    A blocker is anything that delays progress, causes confusion, or requires escalation.
-    Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,
-    resource constraints, lack of approvals, misaligned deadlines etc.
-    Do NOT generate more messages, your role is only to evaluate the conversation!
-"""
 
 # NOTE: for other LLMs (e.g OpenAI),
 # use other LangChain classes.
@@ -39,22 +21,7 @@ model = OllamaLLM(
     temperature=0  # reproducibility
 )
 
+
 async def invoke_async(llm, prompt):
     result = await llm.ainvoke(prompt)
     print(result)
-
-template = PromptTemplate(
-    input_variables=["system", "conversation"],
-    template=dedent(
-        """
-        [SYSTEM]
-        {system}
-
-        [CONVERSATION]
-        {conversation}
-
-        [OUTPUT]
-        {output_format}
-        """
-    )
-)
--- prompts.py
+++ prompts.py
@@ -1,46 +1,114 @@
 # Should be a json but I'm prototyping so this is fine for now.
+from textwrap import dedent  # for a bit better readability
+from langchain.prompts import PromptTemplate
 
-from textwrap import dedent
+# Generic enough so I can use this template for both low and high level summaries.
+template = PromptTemplate(
+    input_variables=["system", "conversation"],
+    template=dedent(
+        """
+        [SYSTEM]
+        {system}
 
+        [CONVERSATION]
+        {conversation}
 
-prompts = {
-    'unified': {
-        'output_format': """Output format ():
-                - UHPAI found: [Yes/No]
-                - Justification: [short text]
+        [OUTPUT]
+        {output_format}
+        """
+    )
+)
 
-                - Blocker found: [Yes/No]
-                - Justification: [short text]""",
-        'system_prompt': dedent("""You are a senior project manager specializing in assembling the Quarterly Business Review.
-                Your task:
-                    1. detect 'Unresolved High-Priority Action Items (UHPAI)' in the conversation!
-                    2. detect potential Blockers in the conversation!
-                Disregard anything non-work related!
-                A 'Unresolved High-Priority Action Item (UHPAI)' is a question or task that have gone unanswered or unaddressed for a significant period.
-                A Blocker is anything that delays progress, causes confusion, or requires escalation.""")
-    },
-    'UHPAI_only': {
-        'output_format': """Output format (always):
-                - UHPAI found: [Yes/No]
-                - Justification: [short text]""",
-        'system_prompt': dedent("""
-                You are a senior project manager specializing in assembling the Quarterly Business Review.
-                Your only task is to detect 'Unresolved High-Priority Action Items (UHPAI)' in the conversation!
-                Disregard anything non-work related.
-                A 'Unresolved High-Priority Action Item (UHPAI)' is a question or task that have gone unanswered or unaddressed for a significant period.
-                Do NOT generate more messages, your role is only to evaluate the conversation!""")
-    },
-    'blocker_only': {
-        'output_format': """Output format (always):
-                - Blocker found: [Yes/No]
-                - Justification: [short text]""",
-        'system_prompt': dedent("""You are a senior project manager specializing in risk detection in project communications.
-                Your only task: detect potential blockers in the conversation.
-                Ignore anything non-work related completely!
-                A blocker is anything that delays progress, causes confusion, or requires escalation.
-                Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,
-                resource constraints, lack of approvals, misaligned deadlines etc.
-                Do NOT generate more messages, your role is only to evaluate the conversation!""")
-    }
+SINGLE_MAIL_SUMMARY_SYS_PROMPT = \
+    dedent("""
+        You are a summarization agent helping a Project Manager by summarizing email conversation chains.
+        Summarize the email conversation below!
+        Follow these principles:
+        Relevant — the summary retains important points and details from the source text;
+        Concise — the summary is information-dense, does not repeat the same point multiple times, and is not unnecessarily verbose;
+        Coherent — the summary is well-structured and easy to follow, not just a jumble of condensed facts;
+        Faithful — the summary does not hallucinate information that is not supported by the source text.
 
-}
+        Focus on capturing / identifying issues like below:
+
+        1. Unresolved High-Priority Action Items (UHPAI): Questions or tasks that have gone \
+        unanswered or unaddressed for a significant period.
+        2. Emerging Risks/Blockers: Potential problems or obstacles identified in \
+        communications that lack a clear resolution path.
+""")
+
+SINGLE_MAIL_OUTPUT_FORMAT = \
+    dedent("""
+Be brief, do not write greeting or intros,
+reply only with the relevant summarization!
+Disregard and do not mention anything not work related,
+e.g parties or holliday plans!
+Answer without any introductory or conclusion text.
+""")
+
+MASTER_SUMMARIZATION_SYS_PROMPT = \
+    dedent("""
+    You are a senior project manager specializing in risk detection in project communications.
+    Your only task: detect potential blockers in the conversation.
+    Disregard anything non-work related.
+    A blocker is anything that delays progress, causes confusion, or requires escalation.
+    Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,
+    resource constraints, lack of approvals, misaligned deadlines etc.
+    Do NOT generate more messages, your role is only to evaluate the conversation!
+""")
+
+MASTER_SUMMARIZATION_OUTPUT_FORMAT = \
+    dedent("""
+    Given the following input, list every instance of the following two issue types:
+    1. Unresolved High-Priority Action Items (UHPAI): Questions or tasks that have gone unanswered or unaddressed for a significant period.
+    2. Emerging Risks/Blockers: Potential problems or obstacles identified in communications that lack a clear resolution path.
+""")
+
+# region legacy
+
+# Disregard.
+# I'll leave it here to pick up useful parts of past prompts.
+
+# prompts = {
+#     'unified': {
+#         'output_format': """Output format ():
+#                 - UHPAI found: [Yes/No]
+#                 - Justification: [short text]
+
+#                 - Blocker found: [Yes/No]
+#                 - Justification: [short text]""",
+#         'system_prompt': dedent("""You are a senior project manager specializing in assembling the Quarterly Business Review.
+#                 Your task:
+#                     1. detect 'Unresolved High-Priority Action Items (UHPAI)' in the conversation!
+#                     2. detect potential Blockers in the conversation!
+#                 Disregard anything non-work related!
+#                 A 'Unresolved High-Priority Action Item (UHPAI)' is a question or task that have gone unanswered or unaddressed for a significant period.
+#                 A Blocker is anything that delays progress, causes confusion, or requires escalation.""")
+#     },
+#     'UHPAI_only': {
+#         'output_format': """Output format (always):
+#                 - UHPAI found: [Yes/No]
+#                 - Justification: [short text]""",
+#         'system_prompt': dedent("""
+#                 You are a senior project manager specializing in assembling the Quarterly Business Review.
+#                 Your only task is to detect 'Unresolved High-Priority Action Items (UHPAI)' in the conversation!
+#                 Disregard anything non-work related.
+#                 A 'Unresolved High-Priority Action Item (UHPAI)' is a question or task that have gone unanswered or unaddressed for a significant period.
+#                 Do NOT generate more messages, your role is only to evaluate the conversation!""")
+#     },
+#     'blocker_only': {
+#         'output_format': """Output format (always):
+#                 - Blocker found: [Yes/No]
+#                 - Justification: [short text]""",
+#         'system_prompt': dedent("""You are a senior project manager specializing in risk detection in project communications.
+#                 Your only task: detect potential blockers in the conversation.
+#                 Ignore anything non-work related completely!
+#                 A blocker is anything that delays progress, causes confusion, or requires escalation.
+#                 Examples: waiting on missing requirements, unresolved dependencies, unclear ownership,
+#                 resource constraints, lack of approvals, misaligned deadlines etc.
+#                 Do NOT generate more messages, your role is only to evaluate the conversation!""")
+#     }
+
+# }
+
+# endregion
